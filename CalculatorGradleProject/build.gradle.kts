fa86eda810c4ac5efa0745f078e8e11c6166d967
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-01-07 17:09:12 -0300

    chore: create project with Gradle and JUnit 5 support

---
+++ CalculatorGradleProject/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+   // aggregator - includes the 3 dependencies below
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    // testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    // testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+    // testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // I included the configuration below to display
+    // err e out messages in the console
+    testLogging.showStandardStreams = true
+}
