f384f227b026b715f5bb48104f8dea57d99181c3
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-01-07 19:25:57 -0300

    style: clean up white spaces and fix indentation

--- CalculatorGradleProject/build.gradle.kts
+++ CalculatorGradleProject/build.gradle.kts
@@ -10,12 +10,11 @@ repositories {
 }
 
 dependencies {
-
    // aggregator - includes the 3 dependencies below
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-    // testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    // testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
-    // testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+   testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+   // testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+   // testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+   // testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
 }
 
 tasks.test {
