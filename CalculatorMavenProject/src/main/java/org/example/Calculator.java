0c0c89b36cd5cba49f7649bc8fb5ab9ca9a176bf
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-01-08 16:08:33 -0300

    feat: add initial subtraction functionality with single test

--- CalculatorMavenProject/src/main/java/org/example/Calculator.java
+++ CalculatorMavenProject/src/main/java/org/example/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public int divide(int dividend, int divisor) {
         return dividend/divisor;
     }
+
+    public int subtract(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
 }
--- CalculatorMavenProject/src/test/java/org/example/CalculatorTest.java
+++ CalculatorMavenProject/src/test/java/org/example/CalculatorTest.java
@@ -19,4 +19,13 @@ class CalculatorTest {
         assertEquals(5, result, "15 divided by 3 did not return 3");
 
     }
+
+    @Test
+    void testSubtraction_withPositiveNumbers_returnsCorrectDifference(){
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result, "5 subtracted by 3 did not return 2");
+
+    }
+
 }
