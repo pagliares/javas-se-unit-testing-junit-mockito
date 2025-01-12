0df4103c12bc77b9c4cf1555fbb9841bd0471c50
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-01-12 11:53:32 -0300

    refactor: change class name

--- CalculatorMavenProject/src/test/java/org/example/RandomMethodOrderExampleTest.java
+++ CalculatorMavenProject/src/test/java/org/example/RandomMethodOrderExampleTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.Random.class)
-public class ExampleRandomMethodOrder {
+public class RandomMethodOrderExampleTest {
 
     @Test
     void testA(){
