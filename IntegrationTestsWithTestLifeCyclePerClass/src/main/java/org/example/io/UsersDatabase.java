e905822166ceafc07c869a930eb35836a39dbb86
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-02-10 20:02:21 -0300

    feat: create first version of integration testing example

---
+++ IntegrationTestsWithTestLifeCyclePerClass/src/main/java/org/example/io/UsersDatabase.java
@@ -0,0 +1,12 @@
+package org.example.io;
+
+import java.util.Map;
+
+public interface UsersDatabase {
+    void init();
+    void close();
+    Map save(String userId, Map userDetails);
+    Map update(String userId, Map user);
+    Map find(String userId);
+    void delete(String userId);
+}
