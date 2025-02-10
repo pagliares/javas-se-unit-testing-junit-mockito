c0f3d1e82075c5763b4eec55e811a822c7c2b197
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-02-10 19:22:54 -0300

    feat: demonstrate how to change the test instance lifecycle from per-method to per-class

--- CalculatorMavenProject/src/test/java/org/example/OrderedByIndexExampleTest.java
+++ CalculatorMavenProject/src/test/java/org/example/OrderedByIndexExampleTest.java
@@ -1,33 +1,54 @@
 package org.example;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+/**
+ * We can change the test lifecycle
+ * By default, it is configured as per-method test lifecycle with the annotation
+ * TestInstance.Lifecycle.PER_METHOD
+ * (one instance of this class for each method in it)
+ * where the execution order is deterministic, but intentionally non-obvious.
+ * Another option is to have a single instance of this test class for all its test methods
+ * by using TestInstance.Lifecycle.PER_CLASS
+ * And this is very helpful when working with integration tests because now your test methods can share
+ * state stored in object instance variables.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // default configuration
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedByIndexExampleTest {
+
+    StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The state of the instance object is : " + completed);
+    }
+
     @Order(1)
     @Test
     void testA(){
         System.out.println("Running test A");
+        completed.append("1");
     }
 
     @Order(4)
     @Test
     void testB(){
         System.out.println("Running test B");
+        completed.append("2");
     }
 
     @Order(3)
     @Test
     void testC(){
         System.out.println("Running test C");
+        completed.append("3");
     }
 
     @Order(2)
     @Test
     void testD(){
         System.out.println("Running test D");
+        completed.append("4");
     }
 }
