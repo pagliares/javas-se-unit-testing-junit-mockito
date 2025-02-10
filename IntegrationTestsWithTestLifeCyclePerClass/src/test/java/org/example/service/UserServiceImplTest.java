9d2c6d7fdb51f7e863866d76db2e455957b103d9
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2025-02-10 20:33:26 -0300

    docs: include comments about how test instance lifecycle impacts static test methods

--- IntegrationTestsWithTestLifeCyclePerClass/src/test/java/org/example/service/UserServiceImplTest.java
+++ IntegrationTestsWithTestLifeCyclePerClass/src/test/java/org/example/service/UserServiceImplTest.java
@@ -6,6 +6,19 @@ import org.junit.jupiter.api.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceImplTest {
 
+    /**
+     * Since there is going to be only one single instance of this class
+     * as configured with @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+     * the methods annotated with @BeforeAll and @AfterAll lifecycle test methods may not be static.
+     * If you remember from previous lessons for these two lifecycle methods to work,
+     * they needed to be static.
+     * If I comment out the test instance annotation and enable the default behavior
+     * where a new instance of a class is created for each test method as configured with
+     * @TestInstance(TestInstance.Lifecycle.PER_METHOD)
+     * Then in that case, the before and after all methods will need to be static.
+     * But for a single instance of a class, or when you use this instance,
+     * lifecycle per class configuration,these two methods do not need to be static.
+     */
     @BeforeAll
     void setup() {
         // Create & initialize database
